6d0d2684ec9638c32ea2bdefb447e51d5dd92e75
Kavindu Gayantha <gayanthakavindu3@gmail.com>
2025-12-29 01:43:05 +0530

    refactor: Change Logic in getTaskById

--- src/main/java/com/kavindu/task_manager/service/TaskService.java
+++ src/main/java/com/kavindu/task_manager/service/TaskService.java
@@ -4,26 +4,34 @@ import com.kavindu.task_manager.model.Task;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class TaskService {
-    private final List<Task> taskList = new ArrayList<>();
+    //private final List<Task> taskList = new ArrayList<>();
+    // We are get arraylist alternative hashmap
+    //key = TaskID(Integer)
+    //value = Task Object
+    private Map<Integer,Task> taskMap = new HashMap<>();
 
     public void addTask(Task task) {
-        taskList.add(task);
+        taskMap.put(task.getId(),task);
     }
 
     public List<Task> getAllTask() {
-        return taskList;
+        return new ArrayList<>(taskMap.values());
     }
 
     public Task getTaskById(int id) {
-        for (Task task: taskList) {
-            if (task.getId() == id) {
-                return task;
-            }
-        }
-        return null;
+//        for (Task task: taskList) {
+//            if (task.getId() == id) {
+//                return task;
+//            }
+//        }
+//        return null;
+
+        return taskMap.get(id);
     }
 }
