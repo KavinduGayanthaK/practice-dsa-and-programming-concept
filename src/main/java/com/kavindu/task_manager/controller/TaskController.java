fd2c7dcc6f12d4f43291fc801004cf38116be8d8
Kavindu Gayantha <gayanthakavindu3@gmail.com>
2025-12-29 18:41:50 +0530

    refactor: Implemented DTO pattern and TaskMapper

--- src/main/java/com/kavindu/task_manager/controller/TaskController.java
+++ src/main/java/com/kavindu/task_manager/controller/TaskController.java
@@ -1,5 +1,6 @@
 package com.kavindu.task_manager.controller;
 
+import com.kavindu.task_manager.dto.TaskDTO;
 import com.kavindu.task_manager.model.Task;
 import com.kavindu.task_manager.service.TaskService;
 import jakarta.validation.Valid;
@@ -18,24 +19,24 @@ public class TaskController {
     }
 
     @PostMapping
-    public String createTask(@Valid @RequestBody Task task) {
-        taskService.addTask(task);
+    public String createTask(@Valid @RequestBody TaskDTO taskDTO) {
+        taskService.addTask(taskDTO);
         return "Task Added Successfully!";
     }
 
     @GetMapping
-    public List<Task> getTasks() {
+    public List<TaskDTO> getTasks() {
         return taskService.getAllTasks();
     }
 
     @GetMapping("/{id}")
-    public Task getTask(@PathVariable int id) {
+    public TaskDTO getTask(@PathVariable int id) {
         return taskService.getTaskById(id);
     }
 
     @PutMapping("/{id}")
-    public Task updateTask(@PathVariable int id, @Valid @RequestBody Task task) {
-        return taskService.updateTask(id,task);
+    public TaskDTO updateTask(@PathVariable int id, @Valid @RequestBody TaskDTO taskDTO) {
+        return taskService.updateTask(id,taskDTO);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/kavindu/task_manager/service/TaskService.java
+++ src/main/java/com/kavindu/task_manager/service/TaskService.java
@@ -1,47 +1,62 @@
 package com.kavindu.task_manager.service;
 
+import com.kavindu.task_manager.dto.TaskDTO;
 import com.kavindu.task_manager.exception.TaskNotFoundException;
 import com.kavindu.task_manager.model.Task;
 import com.kavindu.task_manager.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
 
     // Constructor Injection (Best Practice)
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository, TaskMapper taskMapper) {
         this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
     }
 
-    public void addTask(Task task) {
-        taskRepository.save(task);
+    public TaskDTO addTask(TaskDTO taskDTO) {
+       Task task = taskMapper.toEntity(taskDTO);
+       Task savedTask = taskRepository.save(task);
+       return taskMapper.toDTO(savedTask);
     }
 
-    public List<Task> getAllTasks() {
-        return taskRepository.findAll();
+    public List<TaskDTO> getAllTasks() {
+        List<Task> task = taskRepository.findAll();
+        return task.stream()
+                .map(taskMapper::toDTO)
+                .toList();
+
     }
 
-    public Task getTaskById(int id) {
-        return taskRepository.findById(id).
-                orElseThrow(() -> new TaskNotFoundException("Task not found with id: "+id));
+    public TaskDTO getTaskById(int id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task not found with id: " + id));
+        return taskMapper.toDTO(task);
     }
 
-    public Task updateTask(int id, Task taskDetails) {
-        Task existingTask = getTaskById(id);
+    public TaskDTO updateTask(int id, TaskDTO taskDTO) {
+        Task existingTask = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task not found with id: " + id));
 
-        existingTask.setTitle(taskDetails.getTitle());
-        existingTask.setDescription(taskDetails.getDescription());
-        existingTask.setCompleted(taskDetails.isCompleted());
+        existingTask.setTitle(taskDTO.getTitle());
+        existingTask.setDescription(taskDTO.getDescription());
+        existingTask.setCompleted(taskDTO.isCompleted());
 
-        return taskRepository.save(existingTask);
+        Task savedTask = taskRepository.save(existingTask);
+        return taskMapper.toDTO(savedTask);
     }
 
     public void deleteTask(int id) {
-        Task existingTask = getTaskById(id);
-        taskRepository.delete(existingTask);
+        if (!taskRepository.existsById(id)) {
+            throw new TaskNotFoundException("Task not found with id: " + id);
+        }
+        taskRepository.deleteById(id);
     }
 }
