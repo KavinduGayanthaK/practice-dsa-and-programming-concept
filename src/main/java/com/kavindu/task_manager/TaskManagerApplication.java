d026bba54322d24cfc11914b0f088d30eb9850f3
Kavindu Gayantha <gayanthakavindu3@gmail.com>
2025-12-30 12:35:31 +0530

    chore: Migrated database from H2 to MySQL

--- src/main/java/com/kavindu/task_manager/TaskManagerApplication.java
+++ src/main/java/com/kavindu/task_manager/TaskManagerApplication.java
@@ -1,9 +1,12 @@
 package com.kavindu.task_manager;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Task Manager API", version = "1.0", description = "Documentation for Task Manager Backend"))
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
